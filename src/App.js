7be10966025c9b780c47bc73093c04d58811effb
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-09 22:05:47 +0500

    Appbar is added and dynamic cards are done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import TourCard from './components/TourCard';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import SearchAppBar from './components/SearchAppBar';
+import cities from "./data.json";
 import "./App.css";
+import { Typography } from '@mui/material';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="App" >
+        <SearchAppBar />
+        <Container sx={{marginY: 5}}>
+            {cities.map((city) => (
+                <>
+                <Typography variant="h4" component="h2" marginTop={5} marginBottom={3}>
+                    Top {city.name} Tours
+                </Typography>
+                <Grid container spacing={5}>
+                    {city.tours.map((tour, index) => (
+                        <TourCard tour={tour} key={index}/>
+                    ))}
+                </Grid>
+                </>
+            ))}
+        </Container>
+    </div>
   )
 }
 
