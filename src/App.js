7c519e37a60040465c5045a7e7c02471573badaa
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-10 16:04:15 +0500

    Pages and React router are added to the project

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import TourCard from './components/TourCard';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Tour from './pages/Tour';
 import SearchAppBar from './components/SearchAppBar';
-import cities from "./data.json";
 import "./App.css";
-import { Typography } from '@mui/material';
 
 const App = () => {
   return (
-    <div className="App" >
+    <BrowserRouter>
         <SearchAppBar />
-        <Container sx={{marginY: 5}}>
-            {cities.map((city) => (
-                <>
-                <Typography variant="h4" component="h2" marginTop={5} marginBottom={3}>
-                    Top {city.name} Tours
-                </Typography>
-                <Grid container spacing={5}>
-                    {city.tours.map((tour, index) => (
-                        <TourCard tour={tour} key={index}/>
-                    ))}
-                </Grid>
-                </>
-            ))}
-        </Container>
-    </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:id" element={<Tour />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
